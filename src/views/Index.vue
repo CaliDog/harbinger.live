d293d6c0f3d31999ca5e3425409cad7af5f31f94
Ryan Sears <ryan@calidog.io>
2020-10-17 00:47:25 -0400

    Update copy + add links

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,12 +2,16 @@
   <div class="index is-dark">
     <div class="titles container">
       <h1 class="title has-text-white is-1">Harbinger Oracle</h1>
-      <h2 class="subtitle has-text-white">Defi on the Tezos Blockchain</h2>
+      <h2 class="subtitle has-text-white">Defi on the Tezos Blockchain 🚀</h2>
     </div>
     <div class="container is-flex">
       <div class="box ticker-info">
-        <h2 class="title is-3">Live Oracle Data</h2>
-        <h2 class="subtitle is-6"><a target="_blank" href="https://better-call.dev/mainnet/KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9/operations">View Contract</a></h2>
+        <h2 class="title is-3 has-text-centered">Live Oracle Data</h2>
+        <h2 class="subtitle is-6 has-text-centered links">
+          <a target="_blank" href="https://better-call.dev/mainnet/KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9/operations">View Contract</a>
+          <a target="_blank" href="https://better-call.dev/mainnet/KT1Jr5t9UvGiqkvvsuUbPJHaYx24NzdUwNW9/storage">Contract Data</a>
+          <a target="_blank" href="https://github.com/tacoinfra/harbinger-contracts">Harbinger Github</a>
+        </h2>
         <table class="table is-hoverable is-fullwidth">
           <tbody>
             <tr class="pair-info" :key="pair[0]" v-for="pair in sorted($store.prices)">
@@ -73,6 +77,9 @@ export default {
     }
     .container.is-flex{
       justify-content: center;
+    }
+    .links a{
+      padding: 1rem;
     }
     .ticker-info{
       min-width: 30rem;
